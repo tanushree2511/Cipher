47c75e49da7f3f9e93aa244bffbcbeeb3dafa9b8
Tanushree-2511 <tanushrees.23.becs@acharya.ac.in>
2025-03-24 19:38:46 +0530

    fourth
    Tanushree fourth commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Dashboard from "./pages/Dashboard";
+import PatientDetails from "./pages/PatientDetails";
+import Chatbot from "./pages/Chatbot";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/patients" element={<PatientDetails />} />
+        <Route path="/chatbot" element={<Chatbot />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
