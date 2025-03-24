64c5fa3c23c325a675bd650b6feb739cb138d208
Tanushree-2511 <tanushrees.23.becs@acharya.ac.in>
2025-03-24 22:37:11 +0530

    sixth
    sixth commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Dashboard";
@@ -19,23 +17,7 @@ function App() {
         <Route path="/chatbot" element={<Chatbot />} />
       </Routes>
     </BrowserRouter>
-    <Router>
-      <div className="App">
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
+    
   );
 }
 
